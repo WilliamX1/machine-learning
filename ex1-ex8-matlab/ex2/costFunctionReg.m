befbd8884f7ee64d4a6999faafb96a9650d44617
HuiD <huid@HuiDs-MacBook-Pro.local>
2021-09-20 11:28:02 +0800

    finish logistic regression lab get 100

--- ex1-ex8-matlab/ex2/costFunctionReg.m
+++ ex1-ex8-matlab/ex2/costFunctionReg.m
@@ -17,10 +17,17 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
 
+J_addi = (lambda / (2 * m)) * (sum (theta .* theta) - theta(1) * theta(1));
 
+J = (1 / m) * sum (-y .* log(h) - (1 - y) .* log(1 - h)) + J_addi;
 
+grad_addi = (lambda / m) * theta';
 
+grad = (1 / m) * ((h - y)' * X) + grad_addi;
+
+grad(1) = grad(1) - grad_addi(1);
 
 % =============================================================
 
--- ex1-ex8-matlab/ex2/plotData.m
+++ ex1-ex8-matlab/ex2/plotData.m
@@ -12,13 +12,11 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+pos = find(y == 1);
+neg = find(y == 0);
 
-
-
-
-
-
-
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'Markersize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 % =========================================================================
 
