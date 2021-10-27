c3a5b39c45173bd95b8d2fffb937c80544d0439e
HuiD <huid@HuiDs-MacBook-Pro.local>
2021-10-28 00:24:43 +0800

    finish lab6

--- ex1-ex8-matlab/ex6/dataset3Params.m
+++ ex1-ex8-matlab/ex6/dataset3Params.m
@@ -24,10 +24,22 @@ sigma = 0.3;
 %
 
 
+CC = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmasigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+min_error = 1e9;
 
-
-
-
+for i = 1 : length(CC)
+    for j = 1 : length(sigmasigma)
+        model = svmTrain(X, y, CC(i), @(x1, x2)gaussianKernel(x1, x2, sigmasigma(j)));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        if (error < min_error)
+            min_error = error;
+            C = CC(i);
+            sigma = sigmasigma(j);
+        end
+    end
+end
 
 % =========================================================================
 
